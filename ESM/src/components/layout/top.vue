4cb6eedae85ed4129aaf887a6c0fc9f6193be200
yuqijun <443025314@qq.com>
2019-02-03 10:55:27 +0800

    layout
    layout结构

---
+++ ESM/src/components/layout/top.vue
@@ -0,0 +1,60 @@
+<template>
+  <el-row>
+    <el-col :span="6">
+      <div>
+        <img src="../../assets/images/logo.png">
+      </div>
+    </el-col>
+    <el-col :span="12">
+      <div class="e-h-title">电商后台管理系统</div>
+    </el-col>
+    <el-col :span="6">
+      <div class="e-h-logout">
+        <a href="#" @click.prevent="logout">退出</a>
+      </div>
+    </el-col>
+  </el-row>
+</template>
+
+<script>
+export default {
+  methods:{
+    logout(){
+      this.$confirm('您是否要退出系统，返回登录页面', '提示', {
+          confirmButtonText: '确定',
+          cancelButtonText: '取消',
+          type: 'warning'
+        }).then(() => {
+          window.localStorage.removeItem('roken')
+          this.$router.push('/login')
+          this.$message({
+            type: 'success',
+            message: '退出成功!'
+          });
+        }).catch(() => {
+          this.$message({
+            type: 'info',
+            message: '已取消退出'
+          });          
+        });
+    }
+  }
+};
+</script>
+
+<style>
+/* 头部样式 */
+.e-h-title {
+  text-align: center;
+  color: #fff;
+  font-size: 24px;
+}
+.e-h-logout {
+  text-align: right;
+}
+.e-h-logout a {
+  color: #fff;
+  text-decoration: none;
+  margin-right: 20px;
+}
+</style>
---
+++ ESM/src/components/page/home.vue
@@ -0,0 +1,65 @@
+<template>
+  <!-- 顶部 -->
+  <div class="home">
+    <el-container>
+      <!-- 头部样式 -->
+      <el-header>
+        <Top/>
+      </el-header>
+      <el-container>
+        <!-- 侧边样式 -->
+        <el-aside width="220px">
+          <SideBar/>
+        </el-aside>
+        <!-- 内容样式 -->
+        <el-main>
+          <router-view></router-view>
+        </el-main>
+      </el-container>
+    </el-container>
+  </div>
+</template>
+
+<script>
+// 引入组件
+import Top from "../layout/top";
+import SideBar from "../layout/sideBar";
+export default {
+  components: {
+    Top,
+    SideBar
+  },
+  beforeCreate() {
+    // 得到 token
+    var token = window.localStorage.getItem("token");
+    if (!token) {
+      this.$router.push("/login");
+      this.$message.error("您还没有登录，请先登录");
+      return;
+    }
+  }
+};
+</script>
+
+<style>
+.home,
+.el-container {
+  height: 100%;
+}
+.el-header {
+  padding: 0;
+  line-height: 60px;
+  color: #333;
+  background-color: #b3c0d1;
+}
+.el-aside {
+  background-color: #545c64;
+  color: #333;
+  height: 100%;
+}
+el-main {
+  background-color: #e9eef3;
+  color: #333;
+  height: 100%;
+}
+</style>
